1fc6186fb5254280aed0928617f484758bef6c8d
NikitaER <nik.ershov-34@mail.ru>
2022-06-11 16:13:24 +0300

    Добавление произмедения чисел

---
+++ main1.c
@@ -0,0 +1,12 @@
+1 #include "sdt.h"
+2
+3 // This program is just an example one under VCS.
+4 int main()
+5 {
+6 int a, b;
+7 cout << "Enter A and B: ";
+8 cin >> a >> b;
+9 cout << "A + B = " << a + b << '\n'
+10 << "A - B = " << a - b << '\n'
+11 << "A * B = " << a * b << '\n';
+12 }
